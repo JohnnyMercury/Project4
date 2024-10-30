0a296e640025de1e4b98b8c85069eeb307be534f
Nizar Abushammala <nizatr007@MacBook-Pro-Nizar.local>
2024-10-30 11:51:57 +0100

    Commit

---
+++ src/Employee.java
@@ -0,0 +1,19 @@
+public class Employee implements Person{
+
+    String name;
+    String surname;
+    int age;
+
+    public Employee(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("Name: " + name + " Surname: " + surname + " Age: " + age);
+        int years = 62 - age;
+        System.out.println("The years left to retire are: " + years);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-git//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Student s1 = new Student("1568786581","Nizar","Abushammala",20);
+        Employee e1 = new Employee("Nizar","Abushammala",20);
+
+        s1.getInfo();
+        e1.getInfo();
+
     }
 }
